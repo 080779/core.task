858731d05ce0eed8c7a16852cd6b624c8c18cbd3
深渊 vz <exceed295@hotmail.com>
2019-01-29 18:18:44 +0800

    123123

---
+++ Service/Config/OrderConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Service.Entity;
+
+namespace Service.Config
+{
+    class OrderConfig : IEntityTypeConfiguration<OrderEntity>
+    {
+        public void Configure(EntityTypeBuilder<OrderEntity> builder)
+        {
+            builder.ToTable("tb_orders");
+        }
+    }
+}
